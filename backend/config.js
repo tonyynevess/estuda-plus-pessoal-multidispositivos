875bcaa453c85e65a24a334b1c7805ccebd951bd
tonyynevess <tonyynevess@gmail.com>
2026-09-07 19:09:21 -0300

    Delete backend directory

--- backend/config.js
+++
@@ -1,5 +0,0 @@
-window.ESTUDA_CONFIG = {
-  SUPABASE_URL: "https://vsjquysraysvybnazvod.supabase.co",
-  SUPABASE_KEY: "sb_publishable_36x81R5cV1uL_wtfJy1lgQ_La5-uPnH",
-  API_URL: "https://estudamais-api.onrender.com"
-};
